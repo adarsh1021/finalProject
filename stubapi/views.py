27b1fcdedf28b0d04578aa6aa5aab0ac8bb50634
Adarsh Menon <adarsh1021@gmail.com>
2020-01-27 11:27:53 +0530

    finished api stub

--- stubapi/views.py
+++ stubapi/views.py
@@ -1,7 +1,12 @@
+import pandas as pd
 from django.shortcuts import render, HttpResponse
 
 
+facebookDf = pd.read_csv("stubapi/data/facebook.csv")
+print(facebookDf.head())
+
 # Create your views here.
 def facebook(request):
-    print(request.GET)
-    return HttpResponse("Hello world")
+    fields = request.GET.get("fields", []).split(",")
+    response = facebookDf[fields].to_json()
+    return HttpResponse(response, content_type="application/json")
