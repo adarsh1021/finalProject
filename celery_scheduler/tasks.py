781780c08f944e5d3f76981600c116bf8bd59fbb
Augustine <augustinetharakan12@gmail.com>
2020-02-24 16:07:38 +0530

    scheduler completed

--- celery_scheduler/tasks.py
+++ celery_scheduler/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task, task
 from django.contrib.auth.models import User
-from django.contrib.auth.models import CeleryScheduler
+from celery_scheduler.models import CeleryScheduler
+from backend.models import Data
 
 
 @shared_task
@@ -8,5 +9,10 @@ def fetch_data():
     print("test passed ")
     users = User.objects.all()
     print(users)
-    # query for fetching all the
+
+    schedules_to_rn = CeleryScheduler.objects.filter(date_time)
+
+    for schedule in schedules_to_rn:
+        data = fetch(schedule.endpoint)
+        Data.create(campaign=schedule.id, data=data)
 
